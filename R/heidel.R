9eec2993e025299595f917e896f1cbf3cf3cb861
Georgios <georgios.kazantzidis@roche.com>
2022-06-27 16:32:47 +0200

    jm_post_check

---
+++ R/heidel.R
@@ -0,0 +1,20 @@
+#' Heidelberger and Welch's convergence diagnostic
+#' @param object A JMpost object
+#' @importFrom coda heidel.diag
+#' @importFrom coda geweke.diag
+#' @importFrom coda raftery.diag
+#' @importFrom coda gelman.diag
+#' @importFrom cmdstanr as_mcmc.list
+#' @export
+
+
+jm_post_check <- function(object){
+
+    mcmc_object <- as_mcmc.list(object@cmdstan_fit)
+
+    list( "Heidel" = heidel.diag(mcmc_object),
+          "Gewele" = geweke.diag(mcmc_object),
+          "Raftery" = raftery.diag(mcmc_object),
+          "Gelman" = ifelse( object@options@chains > 1, gelman.diag(mcmc_object), NA_character_))
+    }
+
