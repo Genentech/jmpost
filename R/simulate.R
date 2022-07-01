8d6e197934a0685ef5f9b3b5b304cb266d7f104c
Craig Gower-Page <craig.gower-page@roche.com>
2022-07-01 16:51:16 +0100

    fixed simulation function andded lintr

--- R/simulate.R
+++ R/simulate.R
@@ -27,16 +27,17 @@ dSLD <- function(t, b, s, g, phi) {
 
 
 TTG <- function(b, s, g, phi) {
-    c1 <- log((s * phi) / (g * (1 - phi)))
+    c1 <- log(s / g) + qlogis(phi)
     c2 <- g + s
-    max(0, c1 / c2)
+    c3 <- c1 / c2
+    if_else(c3 < 0, 0, c3)
 }
 
 
 log_haz_loglogistic <- function(t, lambda, p) {
-    c1 <- log(lambda) + log(p) + (p - 1) * log(lambda * t)
-    c2 <- log(1 + (lambda * t)^p)
-    c1 - c2
+    c1 <- lambda * p * (lambda * t)^(p - 1)
+    c2 <- 1 + (lambda * t)^p
+    log(c1 / c2)
 }
 
 
@@ -45,7 +46,7 @@ log_haz_loglogistic <- function(t, lambda, p) {
 #' Simulate OS and SLD data
 #'
 #' @param N Number of patients to simulate
-#' @param lambda_cen Lambda for censoring distribution (1/mean)
+#' @param lambda_cen Lambda for censoring distribution (1/mean in years)
 #' @param lambda Lambda for log-logistic baseline OS distribution
 #' @param p p for log-logistic baseline OS distribution
 #' @param beta link parameter for derivative SLD model
@@ -57,13 +58,12 @@ log_haz_loglogistic <- function(t, lambda, p) {
 #' @param eta_sigma Covariance for the random effects of the SLD parameters
 #' @param beta_age Beta parameter for the age effect
 #' @param beta_sex Beta parameters for sex effect
-#' @param visits Visits in which SLD observations occoured at
-#' @param time_gap Interval time gap to calculate hazard at. In years.
-#' (small numbers = more
-#' accurate distribution but longer run times to simulate data)
+#' @param visits Visits in which SLD observations occoured at (visit 1 = Baseline)
 #' @param time_max Maximum time to calculate hazard at. In years. will error if patient
 #' hasn't died before this time. Excessively large values will increase
 #' the functions run time without adding value
+#' @param pt_suffix A string to attach to the end of the pt name in order to ensure its unique
+#' across multiple runs of the function (for different arms / studies)
 #'
 #' @import dplyr
 #' @importFrom mvtnorm rmvnorm
@@ -73,24 +73,18 @@ log_haz_loglogistic <- function(t, lambda, p) {
 #' @export
 simulate_os_sld <- function(
         N = 200,
-        lambda_cen = 1 / 0.6,
+        lambda_cen = 1 / 90,
         lambda = 0.9,
-        p = 2,
+        p = 2.1,
         beta = 0.0035,
         gamma = -1.0,
-        psi_mu_placebo = c(
+        psi_mu = c(
             "b" = 58,
-            "s" = 3,
+            "s" = 1.5,
             "g" = 0.5,
-            "phi" = 0.2
+            "phi" = 0.3
         ),
-        psi_mu_trt = c(
-            "b" = 58,
-            "s" = 3,
-            "g" = 0.5,
-            "phi" = 0.2
-        ),
-        psi_omega = list(
+        psi_omega = c(
             "b" = 0.135,
             "s" = 0.15,
             "g" = 0.225,
@@ -98,36 +92,47 @@ simulate_os_sld <- function(
         ),
         eta_mu = rep(0, 4),
         eta_sigma = diag(rep(2, 4)),
-        beta_age = 0.1,
-        beta_sex = c("M" = 0, "F" = -0.3),
+        beta_age = 0.2,
+        beta_sex = c("M" = 0.1, "F" = -0.1),
         visits = c(1, 10, 20, 40, 50),
-        time_gap = 1 / 365,
-        time_max = 4
+        time_max = 6,
+        pt_suffix = "suffix"
 ) {
-    unique_pts <- paste0("pt_",
-                         sprintf(
-                             paste0("%0", ceiling(log(N, base = 10)) + 1, "i"),
-                             seq_len(N)
-                         )
+
+    time_gap <- 1 / 365
+    psi_names <- c("b", "s", "g", "phi")
+
+    assert_that(
+        all(psi_names %in% names(psi_omega)),
+        all(psi_names %in% names(psi_mu)),
+        msg = paste(
+            "Either psi_mu or psi_omega don't contain at least",
+            "one of 'b', 's', 'g' or 'phi'"
+        )
+    )
+
+
+    unique_pts <- paste0(
+        "pt_",
+        sprintf(
+            paste0("%0", ceiling(log(N, base = 10)) + 1, "i"),
+            seq_len(N)
+        ),
+        "_",
+        pt_suffix
     )
 
 
     eta <- rmvnorm(N, eta_mu, eta_sigma)
-    colnames(eta) <- names(psi_mu_trt)
+    colnames(eta) <- names(psi_mu)
     eta <- as_tibble(eta) %>% mutate(pt = unique_pts)
 
-    trt_labels <- c(
-        "PLACEBO",
-        "TRT"
-    )
 
     censoring <- tibble(pt = unique_pts) %>%
         mutate(time_cen = rexp(N, lambda_cen) * 365)
 
 
-
     blcovariates <- tibble(pt = unique_pts) %>%
-        mutate(trt = sample(trt_labels, replace = TRUE, size = N)) %>%
         mutate(age = rnorm(N)) %>%
         mutate(sex = factor(
             sample(c("M", "F"), replace = TRUE, size = N),
@@ -140,23 +145,16 @@ simulate_os_sld <- function(
         mutate(chazard_limit = -log(survival))
 
 
-    psi_mu_df <- tibble(
-        var = c(names(psi_mu_placebo), names(psi_mu_trt)),
-        mu = c(psi_mu_placebo, psi_mu_trt),
-        trt = rep(trt_labels, each = length(psi_mu_placebo))
-    )
-
-    psi_omega_df <- tibble(
-        var = names(psi_omega),
-        omega = unlist(psi_omega)
+    psi_df <- tibble(
+        var = psi_names,
+        mu = unlist(psi_mu[psi_names]),
+        omega = unlist(psi_omega[psi_names])
     )
 
 
     sld_pars_meta <- eta %>%
         gather(var, eta, -pt) %>%
-        left_join(select(blcovariates, pt, trt), by = "pt") %>%
-        left_join(psi_mu_df, by = c("var", "trt")) %>%
-        left_join(psi_omega_df, by = "var")
+        left_join(psi_df, by = "var")
 
 
     sld_pars_phi <- sld_pars_meta %>%
@@ -171,7 +169,8 @@ simulate_os_sld <- function(
 
     sld_pars <- bind_rows(sld_pars_nphi, sld_pars_phi) %>%
         select(-mu, -omega) %>%
-        spread(var, eta)
+        spread(var, eta) %>%
+        mutate(ttg = TTG(b, s, g, phi))
 
 
     time_vals <- seq(time_gap, time_max, by = time_gap)
@@ -183,21 +182,22 @@ simulate_os_sld <- function(
         arrange(pt, time) %>%
         left_join(sld_pars, by = "pt") %>%
         mutate(SLD = SLD(time, b, s, g, phi)) %>%
-        left_join(blcovariates, by = c("pt", "trt")) %>%
-        mutate(ttg = TTG(b, s, g, phi)) %>%
-        mutate(dslg = dSLD(time, b, s, g, phi)) %>%
+        left_join(blcovariates, by = "pt") %>%
+        mutate(dslg = dSLD(time_offset, b, s, g, phi)) %>%
         mutate(log_bl_haz = log_haz_loglogistic(time_offset, lambda, p)) %>%
         mutate(log_haz_dsld = beta * dslg) %>%
         mutate(log_haz_ttg = gamma * ttg) %>%
-        mutate(hazard = exp(log_bl_haz + log_haz_cov + log_haz_dsld + log_haz_ttg)) %>%
+        mutate(hazard_per_interval = time_gap * exp(
+            log_bl_haz + log_haz_cov + log_haz_dsld + log_haz_ttg
+        )) %>%
         group_by(pt) %>%
-        mutate(chazard = cumsum(hazard)) %>%
+        mutate(chazard = cumsum(hazard_per_interval)) %>%
         filter(chazard <= chazard_limit) %>%
         mutate(time = time * 365) %>%
         group_by(pt) %>%
         mutate(final = row_number() == n()) %>%
         ungroup() %>%
-        select(pt, time, SLD, age, sex, final) %>%
+        select(pt, time, SLD, age, sex, final, b) %>%
         arrange(pt, time)
 
 
@@ -208,17 +208,28 @@ simulate_os_sld <- function(
         mutate(time = if_else(event == 1, time, time_cen)) %>%
         select(pt, time, age, sex, event)
 
+
+    sld_bl <- sld_pars %>%
+        select(pt, SLD = b) %>%
+        mutate(time = 0)
+
+
     sld_dat <- dat %>%
-        filter(time %in% visits) %>%
+        filter(time %in% (visits - 1)) %>%
+        bind_rows(sld_bl) %>%
         arrange(pt, time) %>%
         left_join(censoring, by = "pt") %>%
         filter(time <= time_cen) %>%
-        select(pt, time, SLD)
+        select(pt, time, SLD) %>%
+        mutate(time = time + 1)
 
     assert_that(
         nrow(os_dat) == N,
+        length(unique(sld_dat$pt)) == N,
         msg = "`time_max` is too small patients have been lost"
     )
 
     return(list(os = os_dat, sld = sld_dat))
 }
+
+
