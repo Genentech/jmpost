414aa643495a5834cafad627d3aa5f234136c8fd
Georgios <georgios.kazantzidis@roche.com>
2022-07-08 16:58:48 +0200

    source_stan_part

---
+++ R/source_stan_part.R
@@ -0,0 +1,12 @@
+#' source_stan_part exports a .stan file as a character
+#' @param filename The name of the .stan file in the directory of the package.
+#' @export
+
+source_stan_part <- function(filename) {
+    absolute_filename <- system.file(
+        "stanparts",
+        filename,
+        package = "jmpost"
+    )
+    readLines(absolute_filename)
+}
