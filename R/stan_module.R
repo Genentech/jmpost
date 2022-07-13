0a872121f8b8b10c0b1fc7a10e617604f2171e40
Georgios <georgios.kazantzidis@roche.com>
2022-07-13 14:35:17 +0200

    stanModule update

--- R/stan_module.R
+++ R/stan_module.R
@@ -23,3 +23,64 @@ StanModule <- setClass(
 )
 
 
+
+#' source_stan_part exports a .stan file as a character
+#' @param filename The name of the .stan file in the directory of the package.
+#' @export
+source_stan_part <- function(filename) {
+    absolute_filename <- system.file(
+        "stanparts",
+        filename,
+        package = "jmpost"
+    )
+    readLines(absolute_filename)
+}
+
+
+#' @importFrom assertthat assert_that
+#' @param functions TODO
+#' @param data TODO
+#' @param parameters TODO
+#' @param transformed_parameters TODO
+#' @param prior TODO
+#' @param generated_quantities TODO
+#' @param inits TODO
+#' @export
+stan_module <- function(functions, data, parameters, transformed_parameters,
+                        prior, generated_quantities, inits ){
+
+    assert_that(
+        is.character(functions),
+        length(functions) == 1,
+        is.character(data),
+        length(data) == 1,
+        is.character(parameters),
+        length(parameters) == 1,
+        is.character(transformed_parameters),
+        length(transformed_parameters) == 1,
+        is.character(gnenerated_quantities),
+        length(gnenerated_quantities) == 1,
+        msg = "`Functions`, `data`, `parameters`, `transformed_parameters` and `gnenerated_quantities` must be length 1 character vectors"
+    )
+
+    assert_that(
+        is.list(prior),
+        is.list(inits),
+        length(inits) == length(prior),
+        msg = "`Prior` and `inits` must be list of the same length"
+    )
+
+    StanModule(functions = source_stan_part(functions),
+               data = source_stan_part(data),
+               parameters = source_stan_part(parameters),
+               transformed_parameters = source_stan_part(transformed_parameters),
+               prior = prior,
+               generated_quantities = source_stan_part(generated_quantities),
+               inits = inits)
+}
+
+
+
+
+
+
