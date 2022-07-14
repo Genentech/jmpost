ddfdc44483e79fbf3321bba5dcc8fa27ac565aac
Georgios <georgios.kazantzidis@roche.com>
2022-07-14 10:04:58 +0200

    Update read_stan_part

--- R/stan_module.R
+++ R/stan_module.R
@@ -1,44 +1,55 @@
 #' StanAll class object
-#' @slot functions Character, the functions part of a stan model
-#' @slot data Character, the data part of a stan model
-#' @slot parameters Character, the parameters part of a stan model
-#' @slot transformed_parameters Character, the transformed_parameters part of a stan model
-#' @slot model Character, the model part of a stan model
-#' @slot prior List, the priors part of a stan model
-#' @slot generated_quantities Character, the generated_quantities part of a stan model
+#' @slot functions Character, the functions part of a stan model.
+#' @slot data Character, the data part of a stan model.
+#' @slot parameters Character, the parameters part of a stan model.
+#' @slot transformed_parameters Character, the transformed_parameters part of a stan model.
+#' @slot model Character, the model part of a stan model.
+#' @slot priors List, the priors part of a stan model.
+#' @slot generated_quantities Character, the generated_quantities part of a stan model.
 #' @slot includes Character
 #' @slot inits List with the initial values of the stan model.
 #' @exportClass StanModule
 StanModule <- setClass(
-    "StanModule",
-    representation(
-        functions = "character",
-        data = "character",
-        parameters = "character",
-        transformed_parameters = "character",
-        prior = "list",
-        generated_quantities = "character",
-        inits = "list"
-    )
+  "StanModule",
+  representation(
+    functions = "character",
+    data = "character",
+    parameters = "character",
+    transformed_parameters = "character",
+    priors = "list",
+    generated_quantities = "character",
+    inits = "list"
+  )
 )
 
 
 
-#' source_stan_part exports a .stan file as a character
-#' @param filename The name of the .stan file in the directory of the package.
+#' read_stan_part returns stan code as a character.
+#' @param file Character, either the absolute path of a stan file, or the name of the stan file in the package directory or the stan code as a string.
 #' @export
-source_stan_part <- function(filename) {
-    if (file.exists(filename)) {
-        absolute_filename <- filename
+read_stan_part <- function(file) {
+    if (file.exists(file)) {
+        absolute_filename <- file
+        out <- readLines(absolute_filename)
+
     } else {
-        absolute_filename <- system.file(
-            "stanparts",
-            filename,
-            package = "jmpost"
-        )
+
+        if (file.exists(system.file("stanparts", file, package = "jmpost"))) {
+            absolute_filename <- system.file(
+                "stanparts",
+                file,
+                package = "jmpost"
+            )
+
+            out <- readLines(absolute_filename)
+
+        } else {
+
+            out <- filename
+        }
     }
 
-    readLines(absolute_filename)
+    return(out)
 }
 
 
@@ -47,47 +58,42 @@ source_stan_part <- function(filename) {
 #' @param data TODO
 #' @param parameters TODO
 #' @param transformed_parameters TODO
-#' @param prior TODO
+#' @param priors TODO
 #' @param generated_quantities TODO
 #' @param inits TODO
 #' @export
 setMethod(
-    f = "initialize",
-    signature = "StanModule",
-    definition = function(.Object, ..., functions, data,
-                          parameters, transformed_parameters,
-                          prior, generated_quantities, inits) {
-        assert_that(
-            is.character(functions),
-            length(functions) == 1,
-            is.character(data),
-            length(data) == 1,
-            is.character(parameters),
-            length(parameters) == 1,
-            is.character(transformed_parameters),
-            length(transformed_parameters) == 1,
-            is.character(gnenerated_quantities),
-            length(gnenerated_quantities) == 1,
-            msg = "`Functions`, `data`, `parameters`, `transformed_parameters` and `gnenerated_quantities` must be length 1 character vectors"
-        )
+  f = "initialize",
+  signature = "StanModule",
+  definition = function(.Object, ..., functions, data,
+                        parameters, transformed_parameters,
+                        priors, generated_quantities, inits) {
+    assert_that(
+      is.character(functions),
+      is.character(data),
+      is.character(parameters),
+      is.character(transformed_parameters),
+      is.character(generated_quantities),
+      msg = "`Functions`, `data`, `parameters`, `transformed_parameters` and `gnenerated_quantities` must be character vectors"
+    )
 
-        assert_that(
-            is.list(prior),
-            is.list(inits),
-            length(inits) == length(prior),
-            msg = "`Prior` and `inits` must be list of the same length"
-        )
+    assert_that(
+      is.list(priors),
+      is.list(inits),
+      length(inits) == length(priors),
+      msg = "`Priors` and `inits` must be list of the same length"
+    )
 
-        callNextMethod(
-            .Object,
-            ...,
-            functions = source_stan_part(functions),
-            data = source_stan_part(data),
-            parameters = source_stan_part(parameters),
-            transformed_parameters = source_stan_part(transformed_parameters),
-            prior = prior,
-            generated_quantities = source_stan_part(generated_quantities),
-            inits = inits
-        )
-    }
+    callNextMethod(
+      .Object,
+      ...,
+      functions = read_stan_part(functions),
+      data = read_stan_part(data),
+      parameters = read_stan_part(parameters),
+      transformed_parameters = read_stan_part(transformed_parameters),
+      priors = priors,
+      generated_quantities = read_stan_part(generated_quantities),
+      inits = inits
+    )
+  }
 )
