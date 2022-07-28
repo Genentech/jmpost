1ed1ba3d1aff88b4ddab51f5ce1dfb9826b6a52a
Georgios <georgios.kazantzidis@roche.com>
2022-07-28 11:41:19 +0200

    Ready for PR update

--- tests/testthat/test-osmodel.R
+++ tests/testthat/test-osmodel.R
@@ -1,19 +1,29 @@
 
 test_that("parametrize filled all the gaps", {
-
     os_mod <- LogLogisticOs()
-    h_link <- HazardLink(stan = StanModule(functions = "bla",
-                                           data = "bla",
-                                           parameters = "bla",
-                                           transformed_parameters = "bla",
-                                           generated_quantities = "bla",
-                                           priors = list(),
-                                           inits = list()),
-                         parameters = "bla",
-                         contribution = "bla")
+    h_link <- HazardLink(
+        stan = StanModule(
+            functions = "bla",
+            data = "bla",
+            parameters = "bla",
+            transformed_parameters = "bla",
+            generated_quantities = "bla",
+            priors = list(),
+            inits = list()
+        ),
+        parameters = "bla",
+        contribution = "bla"
+    )
     os_mod_f <- parametrize(os_mod, h_link)
 
-    all(sapply(c("<", ">"), grepl, os_mod_f@stan@parameters)) != TRUE
-    all(sapply(c("<", ">"), grepl, os_mod_f@stan@transformed_parameters)) != TRUE
-    all(sapply(c("<", ">"), grepl, os_mod_f@stan@generated_quantities)) != TRUE
+    all(sapply(
+        c("link_arguments", "link_log_hazard_contribution", "link_arguments_as_par"),
+        grepl, os_mod_f@stan@functions
+    )) != TRUE
+    all(sapply(c("link_parameters"), grepl, os_mod_f@stan@parameters)) != TRUE
+    all(sapply(
+        c("link_log_surv", "link_log_lik"),
+        grepl, os_mod_f@stan@transformed_parameters
+    )) != TRUE
+    all(sapply(c("link_arguments_as_par"), grepl, os_mod_f@stan@generated_quantities)) != TRUE
 })
