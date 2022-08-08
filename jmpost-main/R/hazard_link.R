98ce99af121a99427db70068f721a3699f452a20
Marco <56521967+charksong@users.noreply.github.com>
2022-08-08 15:39:52 +0800

    Add files via upload

---
+++ jmpost-main/R/hazard_link.R
@@ -0,0 +1,78 @@
+
+
+#' Hazard Link Class
+#'
+#' Creates a Hazard Link object which combines a longditudinal model to
+#' an OS model
+#'
+#' @slot stan A `StanModule` object as created by [StanModule()] that specifies any Stan code
+#' required for the link / contribution
+#' @slot contribution Stan code for what this link contriutes to the log hazard in the OS model
+#' @slot parameter The name of link parameter
+#' @examples
+#' HazardLink(
+#'     parameter = "beta_ttg",
+#'     contribution = "beta_ttg * ttg(phi)",
+#'     stan = StanModule(
+#'         functions = "real ttg(real phi) {phi^2 };"
+#'     )
+#' )
+#' @export
+HazardLink <- setClass(
+    "HazardLink",
+    slots = list(
+        "stan" = "StanModule",
+        "contribution" = "character",
+        "parameters" = "character"
+    )
+)
+
+setValidity("StanModule",function(object){
+    if (!is.character(object@parameters)
+        |!length(object@parameters) >= 1)
+        stop("`parameter` must be a character vector")
+})
+
+setValidity("StanModule",function(object){
+    if (!is.character(object@contribution)
+        |!length(object@contribution) == 1)
+        stop("`contribution` must be length 1 character vectors")
+})
+
+#' @importFrom assertthat assert_that
+#' @rdname StanModule-class
+#' @export
+setMethod(
+    f = "initialize",
+    signature = "HazardLink",
+    definition = function(.Object, ..., stan = StanModule(), contribution, parameters) {
+
+        if (length(stan@parameters) == 0 || all(stan@parameters == "")) {
+            stan@parameters <- sprintf("real %s;", parameters)
+        }
+
+        callNextMethod(
+            .Object,
+            ...,
+            stan = stan,
+            contribution = contribution,
+            parameters = parameters
+        )
+    }
+)
+
+
+#' @rdname merge
+#' @export
+setMethod(
+    f = "merge",
+    signature = c("HazardLink", "HazardLink"),
+    definition = function(x, y) {
+        HazardLink(
+            stan = merge(x@stan, y@stan),
+            contribution = paste0(x@contribution, " + ", y@contribution),
+            parameters = c(x@parameters, y@parameters)
+        )
+    }
+)
+
