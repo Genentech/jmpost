f51ff984902be0318bfcb6f4ed279c6b27ba0255
Georgios <georgios.kazantzidis@roche.com>
2022-08-09 15:15:49 +0200

    add model slot to the stanmodule object. Update as.character

--- R/generics.R
+++ R/generics.R
@@ -12,3 +12,17 @@ setGeneric(
     function(x, y) standardGeneric("merge")
 )
 
+
+
+
+#' h_bracket
+#'
+#' Generic function that creates a character string oriented by curly brackets
+#'
+#' @param x An Object
+#' @export
+setGeneric(
+    "h_bracket",
+    function(x) standardGeneric("h_bracket")
+)
+
