3761aed90a26f01d9269a32e92f795b100489b0b
Marco <s873423774@gmail.com>
2022-08-10 17:51:15 +0800

    Validation Enhancement
    this solves #38

--- R/stan_module.R
+++ R/stan_module.R
@@ -10,16 +10,13 @@
 #' @slot inits List with the initial values of the stan model.
 #' @export
 StanModule <- setClass(
-    "StanModule",
-    representation(
-        functions = "character",
-        data = "character",
-        parameters = "character",
-        transformed_parameters = "character",
-        priors = "list",
-        generated_quantities = "character",
-        inits = "list"
-    )
+    "StanModule",slots = list(functions = "character",
+                              data = "character",
+                              parameters = "character",
+                              transformed_parameters = "character",
+                              priors = "list",
+                              generated_quantities = "character",
+                              inits = "list")
 )
 
 
@@ -41,10 +38,33 @@ read_stan <- function(string) {
     return(out)
 }
 
+#' Validate Inputs
+#'
+#' @param object
+#'
+#' @return StanModule
+#' @export StanModule
+#'
+#' @examples StanModule
+#' @name setValidity
+setValidity("StanModule",function(object){
+    if (!is.character(object@functions)
+        |!is.character(object@data)
+        |!is.character(object@parameters)
+        |!is.character(object@transformed_parameters)
+        |!is.character(object@generated_quantities))
+        stop("`Functions`, `data`, `parameters`, `transformed_parameters`
+       and `generated_quantities` must be character vectors")
+})
 
-#' @importFrom assertthat assert_that
-#' @rdname StanModule-class
-#' @export
+
+setValidity("StanModule",function(object){
+    if (!is.list(object@priors)
+        |!is.list(object@inits))
+        stop("`Priors` and `inits` must be lists")
+})
+
+
 setMethod(
     f = "initialize",
     signature = "StanModule",
@@ -59,25 +79,6 @@ setMethod(
         priors = list(),
         inits = list()
     ) {
-        assert_that(
-            is.character(functions),
-            is.character(data),
-            is.character(parameters),
-            is.character(transformed_parameters),
-            is.character(generated_quantities),
-            msg = paste(
-                "`Functions`, `data`, `parameters`, `transformed_parameters` and",
-                "`generated_quantities` must be character vectors"
-            )
-        )
-
-        assert_that(
-            is.list(priors),
-            is.list(inits),
-            msg = "`Priors` and `inits` must be lists"
-        )
-
-
         callNextMethod(
             .Object,
             ...,
@@ -214,11 +215,11 @@ read_file <- function(filename) {
 
 
 #' Is string a valid file
-#' 
+#'
 #' A utility function to check if a string is a valid file or not.
 #' Used to help address short comings of file.exists that will return TRUE
 #' for a directory as well as a file
-#' 
+#'
 #' @param filename A character string
 is_file <- function(filename = NULL) {
     if (is.null(filename)) {
