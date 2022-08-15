0df372186c83e1d35c8d359831e5cb0ab406a7c2
Craig Gower-Page <craig.gower-page@roche.com>
2022-08-15 16:00:28 +0100

    reformated h_brakcet

--- R/h_bracket.R
+++ R/h_bracket.R
@@ -1,3 +1,4 @@
+
 #' h_bracket
 #'
 #' Generic function that creates a character string oriented by curly brackets
@@ -5,5 +6,11 @@
 #' @param x An Object
 #' @export
 h_bracket <- function(x) {
-    if (any(nchar(x) >= 1, length(x) > 1)) paste0("{\n", paste0(x, collapse = "\n"), "\n}\n")
+    if (any(nchar(x) >= 1, length(x) > 1)) {
+        paste0(
+            "{\n",
+            paste0(x, collapse = "\n"),
+            "\n}\n"
+        )
     }
+}
