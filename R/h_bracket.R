df4a8d2a0057d334254e17619d7474b6bd179138
shawnafang <104414576+shawnafang@users.noreply.github.com>
2022-08-22 15:47:55 +0800

    update h_bracket

---
+++ R/h_bracket.R
@@ -0,0 +1,30 @@
+
+#' h_bracket
+#'
+#' Generic function that creates a character string oriented by curly brackets
+#'
+#' @param x An Object
+#' @export
+h_bracket <- function(x) {
+    if (any(nchar(x) >= 1, length(x) > 1)) {
+
+        str <- paste0(
+            "{\n",
+              paste0("    ",str_map(x), collapse = "\n"),
+            "\n}\n"
+        )
+
+    } else {
+
+        str <- NULL
+
+    }
+    return(str)
+}
+
+#' Function removes spaces and add 4 spaces after change lines
+
+str_map<-function(i){
+    i<-gsub("^\\s*","",i)
+    i<-gsub("\n\\s*","\n    ",i)
+    print(i)}
