1129d5e78d9b7f31bb30e26df03b3da2691511f3
Georgios <georgios.kazantzidis@roche.com>
2022-09-09 11:47:08 +0200

    replace stringi with grepl

--- tests/testthat/test-JmModel.R
+++ tests/testthat/test-JmModel.R
@@ -18,6 +18,6 @@ test_that("Miss-specified stan code. Check the names of the stan files.", {
     my_os <- parametrize(osmod = my_temp_os, link = my_link)
     final_obj <-  as.character(jm_complete(Long = my_long_mod, Os = my_os))
 
-    expect_false(stringi::stri_detect(final_obj, pattern = "/.stan", fixed = TRUE))
+    expect_false(grepl( pattern = "/.stan", final_obj, fixed = TRUE))
 }
 )
