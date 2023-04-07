3ea9e37dad503bc28aa2d3c9a1390ddaacc7d6dc
xiaoyan fang <xiaoyan@roche.com>
2023-04-07 15:32:04 +0800

    log logistic documentary

--- R/SurvivalLoglogistic.R
+++ R/SurvivalLoglogistic.R
@@ -11,14 +11,14 @@ NULL
 
 #' @export
 Survivalloglogistic <- function(
-         lamda = Parameter(prior_lognormal(0,5), init = 1 / 200), ## lognormal distributed prior
+         lambda = Parameter(prior_lognormal(0,5), init = 1 / 200), ## lognormal distributed prior
          p = Parameter(prior_gamma(2, 5), init = 0.5)
 ) {
     .Survivalloglogistic(
         SurvivalModel(
             stan = StanModule("sm-loglogistic/model.stan"),
             parameters = ParameterList(
-                sm_loglogistic_ph_lamda = lamda,
+                sm_loglogistic_ph_lambda = lambda,
                 sm_loglogistic_ph_p =p
             )
         )
