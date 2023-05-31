df1e63d0eec1c2567b7560fdf5633dff1aeb2b6d
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2023-05-31 15:56:29 +0200

    add doc for LongitudinalSamples

--- R/LongitudinalSamples.R
+++ R/LongitudinalSamples.R
@@ -1,5 +1,27 @@
+#' Longitudinal Samples Storage
+#'
+#' This class is an extension of a standard `list` so that we
+#' can define custom methods for it.
+#'
+#' @name LongitudinalSamples
 #' @export
 .LongitudinalSamples <- setClass(
     "LongitudinalSamples",
     contains = "list"
+)
+
+#' @rdname LongitudinalSamples
+#'
+#' @param x (`LongitudinalSamples`)\cr the samples object to subset.
+#' @param i (`vector`)\cr the index vector.
+#'
+#' @return The subsetted `LongitudinalSamples` object.
+#' @export
+setMethod(
+    f = "[",
+    signature = "LongitudinalSamples",
+    definition = function(x, i, ...) {
+        res <- callNextMethod(x, i)
+        .LongitudinalSamples(res)
+    }
 )
