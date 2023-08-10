1d116ef01035610c97ce1f0a172ca5f65bbf565f
Craig Gower-Page <craig.gower-page@roche.com>
2023-08-10 08:46:08 +0100

    Add validation for priors (#198)
    * Added validation methods for priors objects

--- tests/testthat/test-Prior.R
+++ tests/testthat/test-Prior.R
@@ -7,3 +7,35 @@ test_that("Priors work as expected", {
     expect_equal(initialValues(x), 20)
     expect_equal(as.character(x), "normal(4, 10);")
 })
+
+
+test_that("Invalid prior parameters are rejected", {
+    expect_error(
+        prior_normal(0, -1),
+        regexp = "Invalid.*`sigma`"
+    )
+
+    expect_error(
+        prior_beta(-3, 1),
+        regexp = "Invalid.*`a`"
+    )
+
+    expect_error(
+        prior_beta(5, -1),
+        regexp = "Invalid.*`b`"
+    )
+
+    expect_error(
+        prior_gamma(5, -1),
+        regexp = "Invalid.*`beta`"
+    )
+
+    expect_error(
+        prior_lognormal(5, -1),
+        regexp = "Invalid.*`sigma`"
+    )
+
+    # Ensure that validation doesn't wrongly reject priors with no user specified parameters
+    expect_s4_class(prior_none(), "Prior")
+    expect_s4_class(prior_std_normal(), "Prior")
+})
