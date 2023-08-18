2722037ec51d07536ece5d6cd942874dd2c720d8
gowerc <craig.gower-page@roche.com>
2023-08-18 16:18:41 +0100

    added Cache to CICD job

--- tests/testthat/helper-setup.R
+++ tests/testthat/helper-setup.R
@@ -1,4 +1,13 @@
 mcmc_results <- readRDS(test_path("fixtures", "mcmc_results.rds"))
 
-# Set location to store compiled models
-MODEL_DIR <- tempdir()
+# If running in CICD set cache directory to a local directory so that it can be
+# cached for future runs
+is_github_actions <- function() {
+    Sys.getenv("CI") == "true"
+}
+
+if (is_github_actions()) {
+    MODEL_DIR <- file.path(Sys.getenv("HOME"), ".jmpost")
+} else {
+    MODEL_DIR <- tempdir()
+}
