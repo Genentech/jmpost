1a5b691390bc2d535082081851d8496b573b3fde
gowerc <craig.gower-page@roche.com>
2023-10-19 15:23:08 +0100

    print method for quantities

--- tests/testthat/test-Quantities.R
+++ tests/testthat/test-Quantities.R
@@ -162,3 +162,13 @@ test_that("as.data.frame.Quantiles works as expected", {
     )
     expect_equal(actual, expected)
 })
+
+
+test_that("Quantities print method works as expected", {
+    expect_snapshot({
+        raw_x1 <- matrix(1:12, ncol = 3)
+        raw_x2 <- matrix(21:32, ncol = 3)
+        x <- Quantities(list(raw_x1, raw_x2))
+        print(x)
+    })
+})
