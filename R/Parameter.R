031ef752876a29615280565ea0925a3e8112f621
gowerc <craig.gower-page@roche.com>
2024-02-05 16:19:02 +0000

    Resolve Typo

--- R/Parameter.R
+++ R/Parameter.R
@@ -102,7 +102,7 @@ as_stan_list.Parameter <- function(object, ...) {
 #'
 #' @param x (`Paramater`) \cr A model parameter
 #' @param object (`Paramater`) \cr A model parameter
-#' @param ... Not Used.
+#' @param ... Not used.
 #'
 #' @description
 #' Getter functions for the slots of a [`Parameter`] object
