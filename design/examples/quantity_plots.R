53c9d329c42d8ae7d34317726007bf631032a69e
gowerc <craig.gower-page@roche.com>
2024-04-05 16:46:36 +0100

    initial examples

---
+++ design/examples/quantity_plots.R
@@ -0,0 +1,305 @@
+
+library(jmpost)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+
+############################
+#
+# Simulated Data Setup
+#
+#
+
+set.seed(7143)
+
+jlist <- SimJointData(
+    design = list(
+        SimGroup(100, "Arm-A", "Study-X"),
+        SimGroup(100, "Arm-B", "Study-X")
+    ),
+    survival = SimSurvivalExponential(
+        lambda = 1 / (400 / 365),
+        time_max = 3,
+        time_step = 1 / 365,
+        lambda_censor = 1 / 9000,
+        beta_cat = c(
+            "A" = 0,
+            "B" = -0.1,
+            "C" = 0.5
+        ),
+        beta_cont = 0.3
+    ),
+    longitudinal = SimLongitudinalGSF(
+        times = c(
+            -50, -10, -5, -4, -3, -2, -1, 0, 1, 2, 5, 6, 10,
+            25, 30, 40, 50, 75, 80, 100, 110, 125, 150, 175, 200, 225, 250, 275,
+            300, 350, 400, 450, 500, 550, 600, 700, 800, 900,
+            1000, 1100
+        ) / 365,
+        sigma = 0.01,
+        mu_s = c(0.6, 0.4),
+        mu_g = c(0.25, 0.35),
+        mu_b = 60,
+        a_phi = c(20, 15),
+        b_phi = c(15, 20),
+        omega_b = 0.2,
+        omega_s = 0.2,
+        omega_g = 0.2,
+        link_dsld = 0,
+        link_ttg = 0,
+        link_identity = 0
+    ),
+    .silent = TRUE
+)
+
+
+# Ensuring that we have 1 baseline observation per subject and then a
+# random number of post baseline observations
+dat_lm_bl <- jlist@longitudinal |>
+    filter(time <= 10 / 365) |>
+    group_by(pt) |>
+    sample_n(1) |>
+    ungroup()
+
+dat_lm <- jlist@longitudinal |>
+    filter(time > 10 / 365, observed) |>
+    sample_frac(0.25) |>
+    bind_rows(dat_lm_bl) |>
+    arrange(pt, time)
+
+dat_os <- jlist@survival |>
+    filter(pt %in% dat_lm$pt)
+
+
+
+############################
+#
+# Fitting Joint Model
+#
+#
+
+jdat <- DataJoint(
+    subject = DataSubject(
+        data = jlist@survival,
+        subject = "pt",
+        arm = "arm",
+        study = "study"
+    ),
+    survival = DataSurvival(
+        data = dat_os,
+        formula = Surv(time, event) ~ cov_cat + cov_cont
+    ),
+    longitudinal = DataLongitudinal(
+        data = dat_lm,
+        formula = sld ~ time
+    )
+)
+
+jm <- JointModel(
+    longitudinal = LongitudinalGSF(
+        mu_bsld = prior_normal(log(60), 1),
+        mu_ks = prior_normal(log(0.6), 1),
+        mu_kg = prior_normal(log(0.3), 1),
+        omega_bsld = prior_lognormal(log(0.2), 1),
+        omega_ks = prior_lognormal(log(0.2), 1),
+        omega_kg = prior_lognormal(log(0.2), 1),
+        a_phi = prior_lognormal(log(18), 1),
+        b_phi = prior_lognormal(log(18), 1),
+        sigma = prior_lognormal(log(0.01), 1),
+        centred = FALSE
+    ),
+    survival = SurvivalExponential(
+        lambda = prior_lognormal(log(1 / (400 / 365)), 1)
+    ),
+    link = Link()
+)
+
+
+mp <- sampleStanModel(
+    jm,
+    data = jdat,
+    iter_warmup = 500,
+    iter_sampling = 1000,
+    chains = 3,
+    refresh = 100,
+    parallel_chains = 3
+)
+
+
+
+
+stanobj <- as.CmdStanMCMC(mp)
+
+
+
+############################
+#
+# Plot 1 - Predicted vs Observed
+#
+#
+
+longquant_obvs <- LongitudinalQuantities(
+    mp,
+    grid = GridObserved()
+)
+
+dat_quant_obvs <- summary(longquant_obvs) |>
+    as_tibble() |>
+    left_join(dat_lm, by = c("group" = "pt", "time"))
+
+
+ggplot(
+    dat_quant_obvs,
+    aes(x = median, y = sld, xmin = lower, xmax = upper, group = interaction(group, time))
+) +
+    geom_errorbarh(col = "#c6c625") +
+    geom_point(alpha = 0.4) +
+    theme_bw() +
+    geom_abline(intercept = 0, slope = 1, col = "black", lty = "dashed") +
+    xlab("Predicted SLD (mm)") +
+    ylab("Observed SLD (mm)")
+
+
+############################
+#
+# Plot 2 - Predicted vs Observed over time
+#
+#
+
+sampled_subjects <- sample(dat_lm$pt, 4)
+
+longquant_fixed <- LongitudinalQuantities(
+    mp,
+    grid = GridFixed(
+        subjects = sampled_subjects,
+        times = seq(-50, 1100, length.out = 100) / 365
+    )
+)
+
+autoplot(longquant_fixed) +
+    ylab("Observed SLD (mm)") +
+    xlab("Time (years)")
+
+
+## Alternative if you want more customisation
+
+dat_quant_fixed <- summary(longquant_fixed) |>
+    as_tibble()
+
+dat_os_plot <- dat_os |>
+    mutate(group = pt) |>
+    filter(pt %in% sampled_subjects) |>
+    mutate(event_chr = ifelse(event == 1, "Event", "Censored")) |>
+    mutate(event_chr = factor(event_chr, labels = c("Event", "Censored")))
+
+ggplot() +
+    geom_ribbon(
+        data = dat_quant_fixed,
+        aes(x = time, ymin = lower, ymax = upper, group = group),
+        alpha = 0.3,
+        fill = "#df667a"
+    ) +
+    geom_line(
+        data = dat_quant_fixed,
+        aes(x = time, y = median, group = group),
+        col = "#000000"
+    ) +
+    geom_point(
+        data = dat_lm |> mutate(group = pt) |> filter(pt %in% sampled_subjects),
+        aes(x = time, y = sld, group = pt),
+        col = "#000000"
+    ) +
+    geom_vline(
+        data = dat_os_plot,
+        mapping = aes(xintercept = time, lty = event_chr),
+        col = "#a40000"
+    ) +
+    theme_bw() +
+    facet_wrap(~group) +
+    ylab("Observed SLD (mm)") +
+    xlab("Time (years)") +
+    scale_linetype_discrete(name = "")
+
+
+############################
+#
+# Plot 3 - Population Level
+#
+#
+
+gsf_sld <- function(time, b, s, g, phi) {
+    phi <- dplyr::if_else(time >= 0, phi, 0)
+    b * (phi * exp(-s * time) + (1 - phi) * exp(g * time))
+}
+
+samples_df <- stanobj$draws(
+    c("lm_gsf_mu_ks", "lm_gsf_mu_kg", "lm_gsf_mu_bsld", "lm_gsf_a_phi", "lm_gsf_b_phi"),
+    format = "draws_df"
+) |>
+    as_tibble(.name_repair = make.names) |>
+    mutate(sample_id = seq_len(n()))
+
+select_times <- seq(-50, 500, length.out = 100) / 365
+select_times_reduced <- select_times[seq(0, 100, length.out = 8)]
+
+grouped_samples <- tidyr::crossing(
+    samples_df,
+    time = select_times
+) |>
+    mutate(
+        lm_gsf_phi_1 = lm_gsf_a_phi.1. / (lm_gsf_a_phi.1. + lm_gsf_b_phi.1.),
+        lm_gsf_phi_2 = lm_gsf_a_phi.2. / (lm_gsf_a_phi.2. + lm_gsf_b_phi.2.),
+        esld_g1 = gsf_sld(
+            time,
+            exp(lm_gsf_mu_bsld.1.),
+            exp(lm_gsf_mu_ks.1.),
+            exp(lm_gsf_mu_kg.1.),
+            lm_gsf_phi_1
+        ),
+        esld_g2 = gsf_sld(
+            time,
+            exp(lm_gsf_mu_bsld.1.),
+            exp(lm_gsf_mu_ks.2.),
+            exp(lm_gsf_mu_kg.2.),
+            lm_gsf_phi_2
+        )
+    ) |>
+    select(time, esld_g1, esld_g2, sample_id) |>
+    pivot_longer(cols = starts_with("esld"), names_to = "group", values_to = "sld")
+
+grouped_samples_sum <- grouped_samples |>
+    filter(time %in% select_times_reduced) |>
+    group_by(time, group) |>
+    summarise(
+        lower = quantile(sld, 0.025),
+        median = median(sld),
+        upper = quantile(sld, 0.975),
+        .groups = "drop"
+    )
+
+ggplot() +
+    geom_line(
+        data = grouped_samples,
+        mapping = aes(x = time, y = sld, group = interaction(sample_id, group)),
+        alpha = 0.05,
+        col = "#6c9e6c"
+    ) +
+    theme_bw() +
+    facet_wrap(~group) +
+    geom_point(
+        data = grouped_samples_sum,
+        aes(x = time, y = median),
+        col = "#7e1abd"
+    ) +
+    geom_errorbar(
+        data = grouped_samples_sum,
+        aes(x = time, ymin = lower, ymax = upper),
+        width = 0,
+        col = "#7e1abd"
+    ) +
+    geom_line(
+        data = grouped_samples_sum,
+        aes(x = time, y = median),
+        col = "#7e1abd"
+    )
