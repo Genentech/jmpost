2601b87f50a09929bbd00b7e93b75f8852453c48
Isaac Gravestock <isaac.gravestock@roche.com>
2025-07-07 14:13:28 +0200

    update on feedback

--- R/PopulationHR.R
+++ R/PopulationHR.R
@@ -1,80 +1,85 @@
 #' Calculate Population Hazard Ratios
 #'
-#' Calculates hazard ratios marginalised over subject specific random effects
+#' Calculates hazard ratios marginalised over subject specific random effects using the
+#' approach proposed by \insertCite{oudenhoven2020marginal}{jmpost}.
 #'
 #' @param object ([`JointModelSamples`]) \cr samples as drawn from a Joint Model.
-#' @param HR_terms (`character`) \cr to include in the hazard ratio calculation.
-#' By default this include the terms used in the survival model.
+#' @param HR_formula (`formula`) \cr defines the terms to include in the hazard ratio calculation.
+#' By default this uses the right side of the formula used in the survival model.
 #' Set to `NULL` not include any terms
-#' @param arm (`logical`) \cr calculate hazard ratio for `arm`.
-#' @param baseline (`string`) \cr terms to model baseline hazard using variable `"time"`.
-#' Default is a B-spline from [splines]: `"bs(time, df=10)"`
+#' @param baseline (`formula`) \cr terms to model baseline hazard using variable `time`.
+#' Default is a B-spline from [splines]: `~bs(time, df = 10)`
+#' @param quantiles (`numeric`) \cr vector of two values in (0, 1) for calculating quantiles from log hazard ratio
+#' distributions.
+#'
+#' @references \insertAllCited{}
 #'
 #' @returns A matrix
 #'
 #' @importFrom splines bs
-
 PopulationHR <- function(object,
-                         HR_terms = attr(terms(object@data@survival@formula), "term.labels"),
-                         arm = TRUE,
-                         baseline = "bs(time, df=10)") {
+                         HR_formula = object@data@survival@formula,
+                         baseline = ~ bs(time, df=10),
+                         quantiles = c(0.025, 0.975)
+                         ) {
+    assert_class(object, "JointModelSamples")
+    assert_formula(HR_formula)
+    assert_formula(baseline)
+    assert_numeric(quantiles, lower = 0, upper = 1, any.missing = FALSE, unique = TRUE)
+    if (!"time" %in% all.vars(baseline)) stop("baseline formula should include a time term.")
+
     # Extract the variable names used in the data
     subject_var <- object@data@subject@subject
     arm_var <- object@data@subject@arm
     long_time_var <- all.vars(delete.response(terms(object@data@longitudinal@formula)))
     surv_time_var <- all.vars(object@data@survival@formula[[2]][[2]])
-    surv_covs <- all.vars(delete.response(terms(object@data@survival@formula)))
+    surv_covs <- all.vars(delete.response(terms(HR_formula)))
+    if (!all(surv_covs %in% colnames(object@data@survival@data))) {
+        stop("All variables in HR_formula must be in survival data")
+    }
 
-    population_terms <- unique(c(
-        baseline,
-        if (arm) arm_var else NULL,
-        HR_terms
+
+    marginal_formula <- stats::reformulate(c(
+        attr(terms(baseline), "term.labels"),
+        attr(terms(HR_formula), "term.labels")
     ))
-
-    marginal_formula <- reformulate(population_terms)
 
     # Get the survival quantities at the observed longitudinal times
     # and survival event times for each patient:
     times_df <- rbind(
-        object@data@longitudinal@data |>
-            select(subject = {{subject_var}}, time = {{long_time_var}}),
-        object@data@survival@data |>
-            select(subject = {{subject_var}}, time = {{surv_time_var}})
-    ) |>
-        filter(time > 0)
+        stats::setNames(object@data@longitudinal@data[, c(subject_var, long_time_var)], c("subject", "time")),
+        stats::setNames(object@data@survival@data[, c(subject_var, surv_time_var)], c("subject", "time"))
+    ) #|>
+      #  dplyr::arrange(subject, time)
+
+    times_df <- times_df[!duplicated(times_df) & times_df$time > 0, ]
 
     # Generate samples of the log hazard log h_i(t) for each patient i at these time points t.
     grid_spec <- split(times_df$time, times_df$subject)
-    logH <- SurvivalQuantities(
+    log_haz_samples <- SurvivalQuantities(
         object,
         grid = GridManual(grid_spec),
         type = "loghaz"
     )@quantities@quantities |> t()
 
     # Construct \tilde{X} in paper's notation with one row per patient's time
-    W_df <- times_df |>
-        select(subject, time) |>
-        left_join(
-            object@data@subject@data |>
-                select(subject = {{subject_var}}, arm = {{arm_var}}),
-            by = "subject"
-        ) |>
-        left_join(
-            object@data@survival@data |>
-                select(subject = {{subject_var}}, {{surv_covs}}),
-            by = "subject"
-        )
+
+    W_df <- dplyr::left_join(
+        times_df,
+        stats::setNames(object@data@survival@data[, c(subject_var, surv_covs)], c("subject", surv_covs)),
+        by = "subject"
+    )
 
     W_mat <- model.matrix(marginal_formula, W_df)
     # As model matrix contains baseline and covariates, we don't need the intercept term
     # but we want factor variables encoded relative to the intercept
     W_mat <- W_mat[, colnames(W_mat) != "(Intercept)", drop = FALSE]
 
-    estimates <- lm.fit(x = W_mat, y = logH)$coefficients
+    estimates <- stats::lm.fit(x = W_mat, y = log_haz_samples)$coefficients
     tidy_res <- apply(estimates, 1, function(x) {
         c(mean = mean(x),
           median = median(x),
-          quantile(x, c(0.025, 0.975))
+          stats::quantile(x, probs = quantiles)
         )
         }) |> t()
 
---
+++ tests/testthat/test-PopulationHR.R
@@ -0,0 +1,95 @@
+
+test_data_1 <- ensure_test_data_1()
+
+test_that("populationHR works as expected for default parameters", {
+    mp <- test_data_1$jsamples
+
+    result <- suppressWarnings(PopulationHR(object = mp))
+
+    expect_matrix(
+        result[[2]],
+        any.missing = FALSE,
+        ncols = 200,
+        nrows = 13 # 10 baseline spline + 3 covariates
+    )
+
+    # Difference of beta_cat B and C from SimSurvivalExponential()
+    expect_equal(
+        0.2 - -0.4,
+        result[[1]][["cov_catC", "mean"]] - result[[1]][["cov_catB", "mean"]],
+        tolerance = 0.1
+    )
+
+    expect_equal(
+        0.2,
+        result[[1]][["cov_cont", "mean"]],
+        tolerance = 0.1
+    )
+
+    # Summary calculations are match expectations
+    summary_stats <- apply(result[[2]], 1, function(x) c(mean(x), quantile(x, c(0.5, 0.025, 0.975))))
+    expect_equal(
+        unname(t(summary_stats)),
+        unname(result[[1]])
+    )
+})
+
+test_that("populationHR fails for bad input", {
+    mp <- test_data_1$jsamples
+    expect_error(PopulationHR(object = mp@data), "inherit from class")
+    expect_error(PopulationHR(object = mp, HR_formula = "arm"), "formula")
+    expect_error(PopulationHR(object = mp, baseline = ~arm), "time term")
+    expect_error(PopulationHR(object = mp, HR_formula = ~XX), "All variables")
+    expect_error(PopulationHR(object = mp, quantiles = c(-0.3, NA, 2)), "quantiles")
+})
+
+
+test_that("populationHR works as expected for alternative specfications", {
+    mp <- test_data_1$jsamples
+
+    # Arm + continuous covariate
+    result_arm_cont <- PopulationHR(
+        object = mp,
+        baseline = ~splines::ns(time, df = 5),
+        HR_formula = ~arm + cov_cont,
+        quantiles = c(0.05, 0.95)
+    )
+
+    expect_matrix(
+        result_arm_cont[[2]],
+        any.missing = FALSE,
+        ncols = 200,
+        nrows = 7 # 5 baseline spline + 2 covariates
+    )
+
+
+    ### Summary calculations are match expectations
+    summary_stats <- apply(result_arm_cont[[2]], 1, function(x) c(mean(x), quantile(x, c(0.5, 0.05, 0.95))))
+    expect_equal(
+        unname(t(summary_stats)),
+        unname(result_arm_cont[[1]])
+    )
+
+
+    # Arm HR only
+    result_arm <- PopulationHR(
+        object = mp,
+        baseline = ~splines::ns(time, df = 5),
+        HR_formula = ~arm,
+        quantiles = c(0.05, 0.95)
+    )
+
+    expect_matrix(
+        result_arm[[2]],
+        any.missing = FALSE,
+        ncols = 200,
+        nrows = 6 # 5 baseline spline + 1 covariates
+    )
+
+    # Summary calculations are match expectations
+    summary_stats <- apply(result_arm[[2]], 1, function(x) c(mean(x), quantile(x, c(0.5, 0.05, 0.95))))
+    expect_equal(
+        unname(t(summary_stats)),
+        unname(result_arm[[1]])
+    )
+})
