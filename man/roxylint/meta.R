10ad524b60ce93b176318bd96f2d149bfae689be
Daniel Sabanes Bove <daniel@rconis.com>
2026-05-25 14:40:13 +0800

    initial stab which gives default behavior

---
+++ man/roxylint/meta.R
@@ -0,0 +1,8 @@
+list(
+    linters = list(
+        title = roxylint::tidy_title,
+        param = roxylint::tidy_param,
+        return = roxylint::tidy_return,
+        seealso = roxylint::tidy_seealso
+    )
+)
