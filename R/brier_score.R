873ae951ea6b9c594a70410f5978df4f56c12860
Daniel Sabanes Bove <daniel@rconis.com>
2026-05-25 12:36:12 +0800

    rm whitespace

--- R/brier_score.R
+++ R/brier_score.R
@@ -122,7 +122,7 @@ reverse_km_cen_first <- function(t, times, events) {
 #' @typed x: numeric
 #'   a vector for which we want to generate an index so other
 #'   vectors can be put into the same sort order
-#' 
+#'
 #'   Assuming we have a vector that is sorted then this function
 #'   will return the index vector to convert that sorted vector
 #'   into the same sort order as the input vector `x`.
@@ -131,7 +131,7 @@ reverse_km_cen_first <- function(t, times, events) {
 #'   back into `x` we'd need an index vector of `3, 1, 4, 2`.
 #'   This function is used to determine that corresponding
 #'   index vector for an arbitrarily sorted vector `x`.
-#' 
+#'
 #'   There is no specific handling of ties. It is assuming that in the case
 #'   of ties for `x` that the vector you are re-indexing also has tied values
 #'   thus the specific tied element selection does not matter.
