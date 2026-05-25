011896199173e1c365fc1a02a67c5c2494b1ab71
Daniel Sabanes Bove <daniel@rconis.com>
2026-05-25 14:40:52 +0800

    define param linter in line to no longer check sentence case

--- man/roxylint/meta.R
+++ man/roxylint/meta.R
@@ -1,7 +1,9 @@
 list(
     linters = list(
         title = roxylint::tidy_title,
-        param = roxylint::tidy_param,
+        param = function(x, ...) {
+            roxylint::lint_full_stop(x, ...)
+        },
         return = roxylint::tidy_return,
         seealso = roxylint::tidy_seealso
     )
