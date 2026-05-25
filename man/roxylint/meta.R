f25e8edca0fb239c4a482973743a77a0b396b04e
Daniel Sabanes Bove <daniel@rconis.com>
2026-05-25 14:42:30 +0800

    try to add a separate typed linter too but does not help

--- man/roxylint/meta.R
+++ man/roxylint/meta.R
@@ -4,6 +4,9 @@ list(
         param = function(x, ...) {
             roxylint::lint_full_stop(x, ...)
         },
+        typed = function(x, ...) {
+            roxylint::lint_full_stop(x, ...)
+        },
         return = roxylint::tidy_return,
         seealso = roxylint::tidy_seealso
     )
