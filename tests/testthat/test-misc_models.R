a5f31d2c1ccf1d003592e3ca262eb0dfb834f127
Daniel Sabanes Bove <daniel@rconis.com>
2026-07-07 15:57:15 +0800

    add a test for it

--- tests/testthat/test-misc_models.R
+++ tests/testthat/test-misc_models.R
@@ -25,3 +25,41 @@ test_that("Longitudinal Model doesn't print sampler rejection messages", {
         mp
     )))
 })
+
+test_that("Survival model can be fit with a horseshoe prior", {
+    jm <- JointModel(
+        survival = SurvivalWeibullPH(
+            beta = prior_horseshoe(
+                df = 1,
+                df_global = 1,
+                df_slab = 4,
+                scale_global = 0.3,
+                scale_slab = 2
+            )
+        )
+    )
+
+    mp <- run_quietly({
+        sampleStanModel(
+            jm,
+            data = test_data_1$jdata,
+            iter_sampling = 3,
+            iter_warmup = 3,
+            chains = 1,
+            refresh = 0,
+            parallel_chains = 1,
+            seed = 325
+        )
+    })
+
+    draws <- cmdstanr::as.CmdStanMCMC(mp)$draws()
+    variables <- posterior::variables(draws)
+
+    expect_true(all(c(
+        "beta_os_cov[1]",
+        "prior_local_beta_os_cov[1]",
+        "prior_global_beta_os_cov",
+        "prior_slab_beta_os_cov",
+        "prior_scales_beta_os_cov[1]"
+    ) %in% variables))
+})
